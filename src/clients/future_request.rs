3a0f18562415b8adc541f062518efdc99df31ba1
François Mockers <mockersf@gmail.com>
2017-05-02 00:26:10 +0200

    first try at requesting AKC api
    does not handle errors well at all

--- src/clients/future_request.rs
+++ src/clients/future_request.rs
@@ -22,3 +22,19 @@ pub fn get_async(url: Url,
                   })
         .boxed()
 }
+
+macro_rules! read_body {
+    ( $future:expr, $type:ident, $error_message:expr ) => {
+        $future
+        .map(move |mut response| -> Result<$type, serde_json::Error> {
+                 let mut s = String::new();
+                 response.read_to_string(&mut s).unwrap();
+                 let parse = serde_json::from_str(&s);
+                 parse.or_else(|err| {
+                     warn!("error parsing response: {:?}", err);
+                     Err(err)
+                 })
+             })
+        .map_err(|e| Error { msg: format!($error_message, e) })
+    }
+}
